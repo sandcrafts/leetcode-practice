4d09a1d8bccab9d51e830efb941273d09dcd415f
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-04-08 22:21:49 +1000

    GasStation

---
+++ c#/GasStation/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    var solution = new Solution();
+    int[] gas = {1,2,3,4,5}; 
+    int[] cost = {3,4,5,1,2}; 
+
+    Console.WriteLine($"gas: [{string.Join(", ", gas)}]");
+    Console.WriteLine($"cost: [{string.Join(", ", cost)}]");
+    Console.WriteLine("Output: " + solution.Solve(gas, cost));
+  }
+}
