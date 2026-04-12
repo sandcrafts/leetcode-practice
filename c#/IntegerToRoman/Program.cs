65d0728a0c64f537ea6d662d571a4fdc7f6126a7
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-04-12 18:51:19 +1000

    Integer to roman

---
+++ c#/IntegerToRoman/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    var solution = new Solution();
+
+    Console.WriteLine($"Input: Solution.Input)");
+    Console.WriteLine("Output: " + solution.Solve(Solution.Input));
+  }
+}
