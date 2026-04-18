558a0a6695ddaeaa0417507c12ac0c9f9be30255
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-04-18 21:40:08 +1000

    Initial commit for Full justify

---
+++ c#/FullJustify/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    var solution = new Solution();
+    var output = solution.Solve(Solution.InputWords, Solution.MaxWidth);
+    Console.WriteLine($"Input String: [{string.Join(", ", Solution.InputWords)}]");
+    Console.WriteLine($"Input RowCount: {Solution.MaxWidth}");
+    Console.WriteLine($"Output: [{string.Join(", ", output)}]");
+  }
+}
