997d0ec49b63780b9e37634536f5828ee6ce1559
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-04-21 19:52:52 +1000

    first attempt

---
+++ c#/TwoSumTwo/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    var solution = new Solution();
+
+    Console.WriteLine($"InputNumbers: [{string.Join(", ", Solution.InputNumbers)}]");
+    Console.WriteLine($"InputTarget:  {Solution.Target}");
+    Console.WriteLine($"Output: [{string.Join(",", solution.Solve(Solution.InputNumbers, Solution.Target))}]");
+  }
+}
