edf47e433cc4dc7a1c3c0d3cec17ccc442100962
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-04-21 21:19:19 +1000

    optimized solution for two sum two

--- c#/TwoSumTwo/Solution.cs
+++ c#/TwoSumTwo/Solution.cs
@@ -42,6 +42,24 @@ public class Solution {
     public static readonly int Target = 9;
 
     public int[] Solve(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.Length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target){
+                return [left+1, right+1];
+            } else if (sum > target) {
+                right --;
+            } else {
+                left++;
+            }
+        }
+
+        return [];
+    }
+
+        public int[] SolveI(int[] numbers, int target) {
         for (int i = 0; i< numbers.Length; i++)
         {
             for (int j = 1; j < numbers.Length; j++)
