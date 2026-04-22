04681eb5ba7bf89938055cd27626f3ff07070e4c
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-04-22 23:54:56 +1000

    Container with most water

---
+++ c#/ContainerWithMostWater/Solution.cs
@@ -0,0 +1,13 @@
+/*
+11. Container With Most Water
+
+*/
+public class Solution
+{
+    public static readonly int[] Input = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+
+    public int Solve(int[] height)
+    {
+    }
+}
+
