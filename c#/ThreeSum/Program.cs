1555541ddd0c634d5be4bc36a9e10eb253889e25
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-04-23 17:50:16 +1000

    Three sum solution

---
+++ c#/ThreeSum/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    var solution = new Solution();
+
+    Console.WriteLine($"Input: [{string.Join(", ", Solution.Nums)}]");
+    Console.WriteLine("Output: " + solution.Solve(Solution.Nums));
+  }
+}
