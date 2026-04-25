408accac8e512fd432e569e6fe235afe4ab9cc62
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-04-25 20:22:29 +1000

    solution add and last word description update

--- c#/LastWord/Solution.cs
+++ c#/LastWord/Solution.cs
@@ -1,25 +1,33 @@
 /*
 
-42. Trapping Rain Water
+Given a string s consisting of words and spaces, return the length of the last word in the string.
 
-Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+A word is a maximal substring consisting of non-space characters only.
+
+ 
 
 Example 1:
 
-Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
-Output: 6
-Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
 Example 2:
 
-Input: height = [4,2,0,3,2,5]
-Output: 9
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+Example 3:
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
  
 
 Constraints:
 
-n == height.length
-1 <= n <= 2 * 104
-0 <= height[i] <= 105
+1 <= s.length <= 104
+s consists of only English letters and spaces ' '.
+There will be at least one word in s.
 
 */
 public class Solution
