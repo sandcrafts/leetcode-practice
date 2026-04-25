55918168b9807206e79e30e1af8f670232b3e5b8
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-04-25 20:40:03 +1000

    hashset + sliding window solution

--- c#/LongestSubstringWithoutRepeatingCharacter/Solution.cs
+++ c#/LongestSubstringWithoutRepeatingCharacter/Solution.cs
@@ -30,7 +30,30 @@ public class Solution
 {
     public static readonly string Input = "abcabcbb";
 
+    //cleaner solution but only beats 54%
     public int Solve(string s)
+    {
+        int left = 0;
+        HashSet<char> container = new HashSet<char>();
+        int maxLength = 0;
+
+        for (int right = 0; right < s.Length; right++)
+        {
+            while (container.Contains(s[right]))
+            {
+                container.Remove(s[left]);
+                left++;
+            }
+
+            container.Add(s[right]);
+            maxLength = Math.Max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+
+    // faster solution beats 83%
+    public int SolveI(string s)
     {
         int left = 0;
         HashSet<char> container = new HashSet<char>();
