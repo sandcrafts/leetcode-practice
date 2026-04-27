65d02e5a1d2c848405d0e46d0dff067c932459c7
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-04-27 15:22:01 +1000

    optimal solution

--- c#/MinimumWindowSubstring/Solution.cs
+++ c#/MinimumWindowSubstring/Solution.cs
@@ -45,6 +45,61 @@ public class Solution
 
     public string Solve(string s, string t)
     {
+        int start = 0;
+        int minLen = int.MaxValue;
+        int left = 0;
+
+        int substringLength = t.Length;
+        int stringLength = s.Length;
+
+        Dictionary<char, int> map = new Dictionary<char, int>();
+        foreach (char c in t)
+        {
+            map[c] = map.GetValueOrDefault(c, 0) + 1;
+        }
+
+        Dictionary<char, int> window = new Dictionary<char, int>();
+
+        int count = 0;
+
+        for (int right = 0; right < stringLength; right++)
+        {
+            char current = s[right];
+            if (map.ContainsKey(current))
+            {
+                window[current] = window.GetValueOrDefault(current, 0) + 1;
+                if (window[current] <= map[current])
+                {
+                    count++;
+                }
+            }
+
+            while (count == substringLength)
+            {
+                if (right - left + 1 < minLen)
+                {
+                    start = left;
+                    minLen = right - left + 1;
+                }
+
+                char leftChar = s[left];
+                if (map.ContainsKey(leftChar))
+                {
+                    window[leftChar] = window.GetValueOrDefault(leftChar, 0) - 1;
+                    if (window[leftChar] < map[leftChar])
+                    {
+                        count--;
+                    }
+                }
+                left++;
+            }
+        }
+
+        return s.Substring(start, minLen == int.MaxValue ? 0 : minLen);
+    }
+
+    public string SolveI(string s, string t)
+    {
         string str = "";
         StringBuilder strTmp = new StringBuilder();
         int substringLength = t.Length;
@@ -52,22 +107,28 @@ public class Solution
         // int left = 0;
         int count = 0;
         Dictionary<char, int> map = new Dictionary<char, int>();
-        foreach(char c in t) {
+        foreach (char c in t)
+        {
             map[c] = map.GetValueOrDefault(c, 0) + 1;
         }
 
         Dictionary<char, int> window = new Dictionary<char, int>();
 
-        for(int right = 0; right < stringLength; right++) {
-            if (map.ContainsKey(s[right])) {
+        for (int right = 0; right < stringLength; right++)
+        {
+            if (map.ContainsKey(s[right]))
+            {
                 strTmp.Append(s[right]);
                 window[s[right]] = window.GetValueOrDefault(s[right], 0) + 1;
-                if (window[s[right]] <= map[s[right]]) {
-                    count ++;
+                if (window[s[right]] <= map[s[right]])
+                {
+                    count++;
                 }
 
-                if (count == substringLength) {
-                    if (strTmp.Length < str.Length || str == "") {
+                if (count == substringLength)
+                {
+                    if (strTmp.Length < str.Length || str == "")
+                    {
                         str = strTmp.ToString();
                     }
 
@@ -75,7 +136,9 @@ public class Solution
                     window.Clear();
                     count = 0;
                 }
-            } else if (count > 0) {
+            }
+            else if (count > 0)
+            {
                 strTmp.Append(s[right]);
             }
 
