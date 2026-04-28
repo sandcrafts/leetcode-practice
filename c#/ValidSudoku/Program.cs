58a4a60a2d350f1a7cf14afd382e0dec802d5540
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-04-28 17:00:49 +1000

    clear formatting

--- c#/ValidSudoku/Program.cs
+++ c#/ValidSudoku/Program.cs
@@ -4,7 +4,7 @@
   {
     var solution = new Solution();
 
-    Console.WriteLine($"Input:\n[\n " + 
+    Console.WriteLine($"Input: \n[\n " + 
         string.Join(",\n ", Solution.Board.Select(row => $"[{string.Join(",", row)}]"))
         + "\n]"
     );
