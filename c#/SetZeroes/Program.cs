0db21e64e2c8ece94e9db22a2dc62b83b621feb6
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-05-01 20:32:08 +1000

    Set zeroes

---
+++ c#/SetZeroes/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    var solution = new Solution();
+
+    Console.WriteLine($"Input: \n[\n " + 
+        string.Join(",\n ", Solution.Matrix.Select(row => $"[{string.Join(",", row)}]"))
+        + "\n]"
+    );
+
+    solution.Solve(Solution.Matrix);
+
+    Console.WriteLine($"Output: \n[\n " + 
+        string.Join(",\n ", Solution.Matrix.Select(row => $"[{string.Join(",", row)}]"))
+        + "\n]"
+    );
+  }
+}
