67faf1154355d524b1ff3b749935b25c974bedb8
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-05-03 22:58:23 +1000

    canConstruct

---
+++ c#/CanConstruct/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    var solution = new Solution();
+
+    Console.WriteLine($"ransomNote: {Solution.RansomNote}");
+    Console.WriteLine($"Magazine: {Solution.Magazine}");
+    Console.WriteLine("Output: " + solution.Solve(Solution.RansomNote, Solution.Magazine));
+  }
+}
---
+++ c#/CanConstruct/Solution.cs
@@ -0,0 +1,42 @@
+/*
+383. Ransom Note
+
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+ 
+
+Example 1:
+
+Input: ransomNote = "a", magazine = "b"
+Output: false
+Example 2:
+
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+Example 3:
+
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+ 
+
+Constraints:
+
+1 <= ransomNote.length, magazine.length <= 105
+ransomNote and magazine consist of lowercase English letters.
+
+*/
+using System.Text;
+
+public class Solution
+{
+    public static readonly string RansomNote = "aa";
+    public static readonly string Magazine = "aab";
+
+    //Half Solution
+    public bool Solve(string ransomNote, string magazine) {
+        return magazine.Contains(ransomNote);
+    }
+}
+
