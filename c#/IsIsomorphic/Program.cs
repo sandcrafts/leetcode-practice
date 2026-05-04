c902cb6a14133789502b7be5330e05ce39f21d4d
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-05-04 14:56:45 +1000

    Is isomorphic

---
+++ c#/IsIsomorphic/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    var solution = new Solution();
+
+    Console.WriteLine($"ransomNote: {Solution.S}");
+    Console.WriteLine($"Magazine: {Solution.T}");
+    Console.WriteLine("Output: " + solution.Solve(Solution.S, Solution.T));
+  }
+}
