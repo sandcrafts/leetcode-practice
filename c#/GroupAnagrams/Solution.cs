ecfe5e1cb962b7432bf85c34e2766be0c3db78aa
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-05-07 17:39:13 +1000

    final commit for group anagram

--- c#/GroupAnagrams/Solution.cs
+++ c#/GroupAnagrams/Solution.cs
@@ -53,30 +53,29 @@ public class Solution
         foreach(string str in strs) {
             char[] tmpStr = str.ToCharArray();
             
+            Array.Sort(tmpStr);
             
-            for (int i = 0; i < str.Length; i++) {
-                int lowestIndex = i;
+            // for (int i = 0; i < str.Length; i++) {
+            //     int lowestIndex = i;
 
-                for (int j = i + 1; j < str.Length; j++) {
-                    if (tmpStr[j] < tmpStr[lowestIndex]) {
-                        lowestIndex = j;
-                    }
-                }
+            //     for (int j = i + 1; j < str.Length; j++) {
+            //         if (tmpStr[j] < tmpStr[lowestIndex]) {
+            //             lowestIndex = j;
+            //         }
+            //     }
 
-                char current = tmpStr[i];
-                tmpStr[i] = tmpStr[lowestIndex];
-                tmpStr[lowestIndex] = current;
-            }
+            //     char current = tmpStr[i];
+            //     tmpStr[i] = tmpStr[lowestIndex];
+            //     tmpStr[lowestIndex] = current;
+            // }
 
             string sorted = new string(tmpStr);
 
-            if (holder.TryGetValue(sorted, out List<string>? strList)) {
-                strList.Add(str);
-                holder[sorted] = strList;
-            } else
-            {
-                holder[sorted] = new List<string> {str};
-            }
+            if (!holder.ContainsKey(sorted)) {
+                holder[sorted] = new List<string>();
+            } 
+                holder[sorted].Add(str);
+            
         }
 
         foreach(List<string> finalList in holder.Values) {
