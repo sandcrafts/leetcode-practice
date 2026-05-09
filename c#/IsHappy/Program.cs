78ea25308cc43f880490f3cfffc385821aee1c26
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-05-09 10:02:04 +1000

    Is happy initial soln

---
+++ c#/IsHappy/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    var solution = new Solution();
+
+    Console.WriteLine($"N: {Solution.N}");
+    Console.WriteLine("Output: " + solution.Solve(Solution.N));
+  }
+}
