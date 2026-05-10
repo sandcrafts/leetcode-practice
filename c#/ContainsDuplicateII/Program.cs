fc94de000574d9266f297cacf1b613eefa3655ed
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-05-10 22:35:37 +1000

    Contains duplicate II

---
+++ c#/ContainsDuplicateII/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    var solution = new Solution();
+
+    Console.WriteLine($"Input: [{string.Join(", ", Solution.Input)}]");
+    Console.WriteLine($"K: {Solution.K}");
+    Console.WriteLine("Output: " + solution.Solve(Solution.Input, solution.K));
+  }
+}
---
+++ c#/ContainsDuplicateII/Solution.cs
@@ -0,0 +1,40 @@
+/*
+
+219. Contains Duplicate II
+
+Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1], k = 3
+Output: true
+Example 2:
+
+Input: nums = [1,0,1,1], k = 1
+Output: true
+Example 3:
+
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+0 <= k <= 105
+
+*/
+
+public class Solution
+{
+    public static readonly int[] Input = new int[] { 1,2,3,1 };
+    public static readonly int K = 3;
+
+    public bool Solve(int[] nums) {
+
+    }
+}
+
