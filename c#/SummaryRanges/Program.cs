912b232cd4e0040d8ac5c828f4cdf1420b9e05aa
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-05-12 17:02:56 +1000

    summary ranges solution

---
+++ c#/SummaryRanges/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    var solution = new Solution();
+    var output = solution.Solve(Solution.InputNums);
+    Console.WriteLine($"Input String: [{string.Join(", ", Solution.InputNums)}]");
+    Console.WriteLine($"Output: [{string.Join(", ", output)}]");
+  }
+}
