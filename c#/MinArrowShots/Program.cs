1d240cf791d77f96ccb91912191467cdf73aca2f
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-05-17 21:46:46 +1000

    Min arrow shots

---
+++ c#/MinArrowShots/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    var solution = new Solution();
+
+    Console.WriteLine($"Input: \n[\n " + 
+        string.Join(",\n ", Solution.Points.Select(row => $"[{string.Join(",", row)}]"))
+        + "\n]"
+    );
+
+    var output = solution.Solve(Solution.Points);
+
+    Console.WriteLine($"Output: {output}"
+    );
+  }
+}
