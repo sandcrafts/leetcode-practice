084a9dc9626f36241f3000838536ed7018783c6a
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-05-18 16:34:44 +1000

    valid parentheses

---
+++ c#/ValidParentheses/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    var solution = new Solution();
+
+    Console.WriteLine($"Input S: {Solution.S}");
+    Console.WriteLine("Output: " + solution.Solve(Solution.S));
+  }
+}
