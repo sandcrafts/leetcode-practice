43fadcd649934c226c75d368e2355479b5dc303a
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-05-19 19:41:11 +1000

    simplify path

---
+++ c#/SimplifyPath/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    var solution = new Solution();
+
+    Console.WriteLine($"Input Path: {Solution.Path}");
+    Console.WriteLine("Output: " + solution.Solve(Solution.Path));
+  }
+}
---
+++ c#/SimplifyPath/Solution.cs
@@ -0,0 +1,13 @@
+/*
+71. Simplify Path
+*/
+public class Solution
+{
+    public static readonly string Path = "/.../a/../b/c/../d/./";
+
+    public string Solve(string path)
+    {
+    }
+}
+
+    
