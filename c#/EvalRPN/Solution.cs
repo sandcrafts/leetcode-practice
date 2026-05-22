c9514b7f6456ba7325862ffc913263f60521e7c8
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-05-22 23:52:30 +1000

    evaluate reverse polish notation

---
+++ c#/EvalRPN/Solution.cs
@@ -0,0 +1,13 @@
+/*
+150. Evaluate Reverse Polish Notation
+*/
+
+public class Solution 
+{
+    public static readonly string[] Input = new int[] {"2","1","+","3","*"};
+    
+    public int Solve(string[] prices) {
+
+    }
+}
+
