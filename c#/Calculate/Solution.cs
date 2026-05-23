7fe66d0cecf830e804333111579fa60ff8061242
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-05-23 23:18:23 +1000

    basic calculator

---
+++ c#/Calculate/Solution.cs
@@ -0,0 +1,17 @@
+/*
+
+224. Basic Calculator
+
+
+
+*/
+public class Solution
+{
+    public static readonly string Input = "(1+(4+5+2)-3)+(6+8)";
+
+    public int Solve(string s)
+    {
+        
+    }
+}
+
