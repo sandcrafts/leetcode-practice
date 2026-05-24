9dfa90e48047520950ffbfcdef9638fec0a7fcf2
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-05-24 23:27:36 +1000

    solution file

--- c#/LinkedListCycle/ListNode.cs
+++ c#/LinkedListCycle/ListNode.cs
@@ -1,9 +1,7 @@
-
-//Definition for singly-linked list.
 public class ListNode
 {
   public int val;
-  public ListNode next;
+  public ListNode? next;
   public ListNode(int x)
   {
     val = x;
--- c#/LinkedListCycle/Program.cs
+++ c#/LinkedListCycle/Program.cs
@@ -4,7 +4,17 @@
   {
     var solution = new Solution();
 
+    ListNode head = new ListNode(3);
+    ListNode one = new ListNode(2);
+    ListNode two = new ListNode(0);
+    ListNode three = new ListNode(-4);
+
+    head.next = one;
+    one.next = two;
+    two.next = three;
+    three.next = one;
+
     Console.WriteLine($"Input: head = [3,2,0,-4], pos = 1");
-    Console.WriteLine("Output: " + solution.Solve(Solution.N));
+    Console.WriteLine("Output: " + solution.Solve(head));
   }
 }
