d85690c6b2032889c2764893e76208caba570fad
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-05-25 23:54:55 +1000

    copy

---
+++ c#/LinkedListCycle copy/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    var solution = new Solution();
+
+    ListNode head = new ListNode(3);
+    ListNode one = new ListNode(2);
+    ListNode two = new ListNode(0);
+    ListNode three = new ListNode(-4);
+
+    head.next = one;
+    one.next = two;
+    two.next = three;
+    three.next = one;
+
+    Console.WriteLine($"Input: head = [3,2,0,-4], pos = 1");
+    Console.WriteLine("Output: " + solution.Solve(head));
+  }
+}
