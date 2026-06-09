8c9c89d5a7ea7225476913d57519822fa9f7a06f
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-06-09 19:46:53 +1000

    Reverse linked llist in k group

---
+++ c#/ReverseLinkedListInKGroup/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    var solution = new Solution();
+
+    ListNode head = new ListNode(1);
+    ListNode one = new ListNode(2);
+    ListNode two = new ListNode(3);
+    ListNode three = new ListNode(4);
+    ListNode four = new ListNode(5);
+
+    head.next = one;
+    one.next = two;
+    two.next = three;
+    three.next = four;
+    int k = 3;
+
+
+    Console.WriteLine($"Input: head = [1,2,3,4,5], k = 3");
+    ListNode? output = solution.Solve(head, k);
+
+    Console.WriteLine($"Output: {string.Join(",", output!.ToArray())}");
+  }
+}
---
+++ c#/ReverseLinkedListInKGroup/Solution.cs
@@ -0,0 +1,12 @@
+/*
+25. Reverse Nodes in k-Group
+
+*/
+
+public class Solution
+{
+    public ListNode? Solve(ListNode head, int k)
+    {
+    }
+}
+
