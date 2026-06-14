5019032b284ddb8ff901c7f170078fd0171531b9
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-06-14 23:56:09 +1000

    remove nts node from end of list

---
+++ c#/RemoveNthNodeFromEndOfList/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    var solution = new Solution();
+
+    ListNode head = new ListNode(1);
+    ListNode one = new ListNode(2);
+    ListNode two = new ListNode(3);
+    ListNode three = new ListNode(4);
+    ListNode four = new ListNode(5);
+
+    head.next = one;
+    one.next = two;
+    two.next = three;
+    three.next = four;
+    int n = 2;
+
+
+    Console.WriteLine($"Input: head = [1,2,3,4,5], n = 2");
+    ListNode? output = solution.Solve(head, n);
+
+    Console.WriteLine($"Output: {string.Join(",", output!.ToArray())}");
+  }
+}
---
+++ c#/RemoveNthNodeFromEndOfList/Solution.cs
@@ -0,0 +1,11 @@
+/*
+19. Remove Nth Node From End of List
+*/
+
+public class Solution
+{
+    public ListNode? Solve(ListNode head, int k)
+    {
+    }
+}
+
