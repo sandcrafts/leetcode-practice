be7216226eac4af7e946696fa69d423fc948c19d
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-06-15 21:29:36 +1000

    RemoveDuplicatesFromSortedListII

---
+++ c#/RemoveDuplicatesFromSortedListII/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    var solution = new Solution();
+
+    ListNode head = new ListNode(1);
+    ListNode one = new ListNode(2);
+    ListNode two = new ListNode(3);
+    ListNode three = new ListNode(3);
+    ListNode four = new ListNode(4);
+    ListNode five = new ListNode(4);
+    ListNode six = new ListNode(5);
+
+    head.next = one;
+    one.next = two;
+    two.next = three;
+    three.next = four;
+    four.next = five;
+    five.next = six;
+
+
+    Console.WriteLine($"Input: head = [1,2,3,3,4,4,5]");
+    ListNode? output = solution.Solve(head);
+
+    Console.WriteLine($"Output: {string.Join(",", output!.ToArray())}");
+  }
+}
---
+++ c#/RemoveDuplicatesFromSortedListII/Solution.cs
@@ -0,0 +1,15 @@
+/*
+82. Remove Duplicates from Sorted List II
+
+
+
+*/
+
+public class Solution
+{
+    public ListNode? Solve(ListNode head)
+    {
+        return head;
+    }
+}
+
