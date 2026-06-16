3fb57b31ebd6d09c182631e35bb81d0208172e3a
Sandeep Bhattarai <sandeepbhattarai.is@gmail.com>
2026-06-17 01:24:59 +1000

    rotate list

--- c#/RotateList/Solution.cs
+++ c#/RotateList/Solution.cs
@@ -7,6 +7,37 @@ public class Solution
 {
     public ListNode? Solve(ListNode head, int k)
     {
+        if (head == null || head.next == null || k == 0)
+        {
+            return head;
+        }
+
+        ListNode dummy = head;
+        ListNode newTail = head;
+        int counter = 1;
+
+        while (dummy.next != null)
+        {
+            dummy = dummy.next;
+            counter++;
+        }
+        k %= counter;
+        if (k == 0)
+            return head;
+
+        int steps = counter - k - 1;
+
+        for (int i = 1; i <= steps; i++)
+        {
+            newTail = newTail.next;
+        }
+
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+        dummy.next = head;
+
+
+        return newHead;
     }
 }
 
